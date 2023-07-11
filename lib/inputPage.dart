d7a3eea437b01f3c6b5c8c73b23a67f3bb93be80
shreshtha-132 <shreshthakumargupta@gmail.com>
2023-07-11 16:09:14 +0530

    first commit
    -> used theme to create a theme of own.
    -> made the UI using rows, columns and expanded widgets, made the ambiguous code into resusable class.

---
+++ lib/inputPage.dart
@@ -0,0 +1,66 @@
+// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('BMI CALCULATOR')),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(Color(0xFF1D1E33)),
+                  ),
+                  Expanded(
+                    child: ReusableCard(Color(0xFF1D1E33)),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(Color(0xFF1D1E33)),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(Color(0xFF1D1E33)),
+                  ),
+                  Expanded(
+                    child: ReusableCard(Color(0xFF1D1E33)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  late Color colour;
+  ReusableCard(this.colour);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
