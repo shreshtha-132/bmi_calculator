a502e417f5e40df7ebc396fad562b1435fc54280
shreshtha-132 <shreshthakumargupta@gmail.com>
2023-07-12 10:34:47 +0530

    container and reusable card commit.
    -> used reusable card for the ui.
    -> used a container at last for the buttons, kept the width as double.infinity for to fit any screen, and stretch given space.

--- lib/inputPage.dart
+++ lib/inputPage.dart
@@ -12,40 +12,48 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text('BMI CALCULATOR')),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ReusableCard(Color(0xFF1D1E33)),
-                  ),
-                  Expanded(
-                    child: ReusableCard(Color(0xFF1D1E33)),
-                  ),
-                ],
-              ),
+      appBar: AppBar(
+        title: Center(child: Text('BMI CALCULATOR')),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableCard(Color(0xFF1D1E33)),
+                ),
+              ],
             ),
-            Expanded(
-              child: ReusableCard(Color(0xFF1D1E33)),
+          ),
+          Expanded(
+            child: ReusableCard(Color(0xFF1D1E33)),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableCard(Color(0xFF1D1E33)),
+                ),
+              ],
             ),
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ReusableCard(Color(0xFF1D1E33)),
-                  ),
-                  Expanded(
-                    child: ReusableCard(Color(0xFF1D1E33)),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ));
+          ),
+          Container(
+            height: 60,
+            width: double.infinity,
+            color: Color(0xFFEB1555),
+            margin: EdgeInsets.only(top: 10),
+            // child: Text('Last Container'),
+          )
+        ],
+      ),
+    );
   }
 }
 
